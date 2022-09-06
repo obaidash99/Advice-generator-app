acef823896e764a0f8a99e54f6816bbf70225248
obaida <obaidashurbaji99@gmail.com>
2022-09-06 16:20:32 +0200

    more js

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const quote = document.querySelector('.content q');
 const dice = document.querySelector('.dice');
 
-fetch('https://api.adviceslip.com/advice')
-	.then((response) => {
-		return response.json();
-	})
-	.then((advice) => {
-		// advice is an object contains objects
-		const adviceObj = advice.slip; // slip is the chil object
-		quote.innerHTML = adviceObj.advice;
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+window.onload = () => {
+	getAdvice();
+};
+dice.addEventListener('click', () => {
+	getAdvice();
+});
+
+function getAdvice() {
+	fetch(' https://api.adviceslip.com/advice')
+		.then((response) => {
+			return response.json();
+		})
+		.then((advice) => {
+			// advice is an object contains objects
+			const idSpan = document.querySelector('.content span');
+			const adviceObj = advice.slip; // slip is the child object
+			idSpan.innerHTML = `#${adviceObj.id}`;
+			quote.innerHTML = adviceObj.advice;
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
